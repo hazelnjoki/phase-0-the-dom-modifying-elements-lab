ca6559e88bed43ba33ef6d24335a5254c752e29d
hazelnjoki <muthoninjoki2003@gmail.com>
2023-10-18 08:50:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.parentNode.removeChild(mainElement);
+
+const oldHeader = document.getElementById('oldHeader');
+const newHeader = document.createElement('h1');
+const name = "Hazel Njoki"; // Replace this with your actual name
+newHeader.textContent = name + ' is the champion!';
+newHeader.id = 'victory';
+oldHeader.parentNode.replaceChild(newHeader, oldHeader);
